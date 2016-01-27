2af833cbc17f7326c982abdde6b8c9aed9a184c3
jasimea <jasimea@gmail.com>
2016-01-27 16:49:48 +0400

    Finished Building and clear the source code

--- services/routes.js
+++
@@ -1,7 +0,0 @@
-
-export var Routes = {
-    schedule: { id: 'schedule', title: 'Schedule'},
-    meeting: { id: 'meeting', title: 'Meetings' },
-    settings: { id: 'settings', title: 'Settings' },
-    search: { id: 'search', title: 'Search' }
-};
